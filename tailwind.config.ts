b06915919bb4bb2adffcb0bf005d9b66da887c93
dontknowunknown57-cloud <dontknowunknown57@gmail.com>
2025-08-12 22:37:51 +0545

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,7 @@ export default {
 					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
 					border: 'hsl(var(--sidebar-border))',
 					ring: 'hsl(var(--sidebar-ring))'
-				}
+				},
 				brand: {
 					DEFAULT: 'hsl(var(--brand))',
 					2: 'hsl(var(--brand-2))'
